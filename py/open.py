c8626514bf4e1d2c3dc0c854d9c63d4a1ec00d0f
unknown <ZX@FZ-LINFEI-YE.fzzixun.com>
2022-04-13 15:51:15 +0800

    feat: 打开chrome

--- py/open.py
+++ py/open.py
@@ -1,4 +1,11 @@
 
+import os
 import webbrowser
-chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+if (not os.path.exists(chrome_path)):
+    chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+chrome_path += ' %s'
+
+print(chrome_path)
 webbrowser.get(chrome_path).open('https://www.baidu.com/')
