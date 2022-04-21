46f091c1a621b6500a9a5dfa06b2e3d572f2590e
unknown <ZX@FZ-LINFEI-YE.fzzixun.com>
2022-04-21 11:41:00 +0800

    操作alert

---
+++ py/alert_auto.py
@@ -0,0 +1,32 @@
+import psutil
+from pywinauto.application import Application
+
+# pl = psutil.pids()
+# p = psutil.Process(15236)
+# print(pl)
+# print(p)
+
+# app = Application().connect(path="C:\Program Files\Google\Chrome\Application\chrome.exe")
+# tab = app.window(title='新标签页')
+# tab.type_keys('{F6}')
+# tab.type_keys('{ESC}')
+# tab.type_keys('www.google.com')
+# tab.type_keys('{ENTER}')
+
+# app.Window_()
+# print(app)
+
+
+app = Application(backend='uia').connect(title_re=".*Chrome.*")
+print(app)
+
+# dlg = app.top_window()
+# print(dlg)
+
+dlg = app.window(title_re=".*", control_type="Pane")
+
+# dlg.print_control_identifiers(depth=5)
+dlg.child_window(title="确定", control_type="Button").click()
+
+# ctrl = dlg.child_window(title="poopcode.com 显示", control_type="Custom")
+# ctrl.print_control_identifiers()
